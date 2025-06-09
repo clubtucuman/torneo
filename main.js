b6e009d6e58dee005b982a9fb655a9b89e39fe36
clubtucuman <andradaignacio07@gmail.com>
2025-06-09 00:04:43 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,30 +2,31 @@ const equiposData = {
   A: {
     zonas: {
       A1: [
-        { nombre: "Equipo A1-1", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
-        { nombre: "Equipo A1-2", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
-        { nombre: "Equipo A1-3", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
-        { nombre: "Equipo A1-4", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
-        { nombre: "Equipo A1-5", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
-        { nombre: "Equipo A1-6", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
-        { nombre: "Equipo A1-7", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
-        { nombre: "Equipo A1-8", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
-        { nombre: "Equipo A1-9", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
-        { nombre: "Equipo A1-10", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
-        { nombre: "Equipo A1-11", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "EL Acceso", puntos: 3, pj: 1, pg: 1, pe: 0, pp: 0, gf: 7, gc: 3, dg: +4 },
+        { nombre: "Chaco", puntos: 3, pj: 1, pg: 1, pe: 0, pp: 0, gf: 2, gc: 1, dg: +1 },
+        { nombre: "Zapiola", puntos: 1, pj: 1, pg: 0, pe: 1, pp: 0, gf: 3, gc: 3, dg: 0 },
+        { nombre: "Lo de Beto", puntos: 1, pj: 1, pg: 0, pe: 1, pp: 0, gf: 3, gc: 3, dg: 0 },
+        { nombre: "Los Eucaliptus", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Motorhead", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "La Noite", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "As de Basto", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "La Nueva Guardia", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Centenario", puntos: 0, pj: 1, pg: 0, pe: 0, pp: 1, gf: 1, gc: 2, dg: -1 },
+        { nombre: "La 2002", puntos: 0, pj: 1, pg: 0, pe: 0, pp: 1, gf: 3, gc: 7, dg: -4 },
         // ... hasta Equipo A1-10
       ],
       A2: [
-        { nombre: "Equipo A2-1", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
-        { nombre: "Equipo A2-2", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
-        { nombre: "Equipo A2-3", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
-        { nombre: "Equipo A2-4", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
-        { nombre: "Equipo A2-5", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
-        { nombre: "Equipo A2-6", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
-        { nombre: "Equipo A2-7", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
-        { nombre: "Equipo A2-8", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
-        { nombre: "Equipo A2-9", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
-        { nombre: "Equipo A2-10", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Semen Up", puntos: 3, pj: 1, pg: 1, pe: 0, pp: 0, gf: 4, gc: 3, dg: +1 },
+        { nombre: "Villa Primavera", puntos: 3, pj: 1, pg: 1, pe: 0, pp: 0, gf: 3, gc: 2, dg: +1 },
+        { nombre: "Perro Viejo", puntos: 3, pj: 1, pg: 1, pe: 0, pp: 0, gf: 2, gc: 1, dg: +1 },
+        { nombre: "La Roma", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "El JRS", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Jarabillos", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Salen Brasas", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Jardin Espacial", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Ateno Yiyo", puntos: 0, pj: 1, pg: 0, pe: 0, pp: 1, gf: 3, gc: 4, dg: -1 },
+        { nombre: "Monoblock 420", puntos: 0, pj: 1, pg: 0, pe: 0, pp: 1, gf: 2, gc: 3, dg: -1 },
+        { nombre: "Barreme la Veredita", puntos: 0, pj: 1, pg: 0, pe: 0, pp: 1, gf: 1, gc: 2, dg: -1 },
         // ... hasta Equipo A2-10
       ]
     },
@@ -49,6 +50,14 @@ const equiposData = {
       B2: [
         { nombre: "Equipo B2-1", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
         { nombre: "Equipo B2-2", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Equipo B2-3", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Equipo B2-4", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Equipo B2-5", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Equipo B2-6", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Equipo B2-7", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Equipo B2-8", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Equipo B2-9", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Equipo B2-10", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
         // ... hasta Equipo B2-10
       ]
     },
@@ -59,11 +68,27 @@ const equiposData = {
       C1: [
         { nombre: "Equipo C1-1", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
         { nombre: "Equipo C1-2", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Equipo C1-3", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Equipo C1-4", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Equipo C1-5", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Equipo C1-6", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Equipo C1-7", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Equipo C1-8", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Equipo C1-9", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Equipo C1-10", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
         // ... hasta Equipo C1-10
       ],
       C2: [
         { nombre: "Equipo C2-1", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
         { nombre: "Equipo C2-2", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Equipo C2-3", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Equipo C2-4", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Equipo C2-5", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Equipo C2-6", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Equipo C2-7", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Equipo C2-8", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Equipo C2-9", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Equipo C2-10", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
         // ... hasta Equipo C2-10
       ]
     },
@@ -71,14 +96,32 @@ const equiposData = {
   },
   "+38": {
     zonas: {
-      "+38A": [
-        { nombre: "Equipo +38A-1", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
-        { nombre: "Equipo +38A-2", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+      "+38-A": [
+        { nombre: "Motorhead 38", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Chaco 38", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Los Toledos", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Barreme la Veredita 38", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Huracanes de Bernal", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "El Ciclon", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Olivieri", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Fuerte Apache", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Bumbi 4D", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Los Euka 38", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Dos Avenidas", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
         // ... hasta Equipo +38A-10
       ],
-      "+38B": [
-        { nombre: "Equipo +38B-1", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
-        { nombre: "Equipo +38B-2", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+      "+38-B": [
+        { nombre: "Centenario 38", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Chuker", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Chumbimba Srs", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Lo de Beto", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Sector 7G", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "La Braschineta", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Fiorentina", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "El Astor Villa", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "El Rejunte", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Los Andes", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "4 De Copas 38", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
         // ... hasta Equipo +38B-10
       ]
     },
@@ -87,9 +130,18 @@ const equiposData = {
   },
   "+45": {
     equipos: [
-      { nombre: "Equipo +45-1", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
-      { nombre: "Equipo +45-2", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
-      // ... hasta Equipo +45-10
+        { nombre: "Olivieri 45", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "La Barra", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Athletic Bilbao", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Astor Villa 45", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Motorhead 45", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Ediq", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Motor V 45", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Nueva Roma", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Barreme la Veredita 45", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Lo de Beto 45", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "4 De Copas", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
+        { nombre: "Centenario 45", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0, dg: 0 },
     ],
     descensos: false
   }
@@ -128,24 +180,39 @@ document.querySelectorAll('.section').forEach(s => s.style.display = 'none');
 
 const goleadoresPorCategoria = {
   'A': [
-    { jugador: 'Lionel Messi', equipo: 'La Catedral', goles: 18 },
-    { jugador: 'Juan Pérez', equipo: 'Los Leones', goles: 15 },
+    { jugador: '', equipo: 'La Catedral', goles: 0 },
+    { jugador: '', equipo: 'La Catedral', goles: 0 },
+    { jugador: '', equipo: 'La Catedral', goles: 0 },
+    { jugador: '', equipo: 'La Catedral', goles: 0 },
+    { jugador: '', equipo: 'Los Leones', goles: 0  },
   ],
   'B': [
-    { jugador: 'Carlos Gómez', equipo: 'Los Guerreros', goles: 12 },
-    { jugador: 'Andrés Silva', equipo: 'El Rejunte', goles: 10 },
+    { jugador: '', equipo: 'La Catedral', goles: 0 },
+    { jugador: '', equipo: 'La Catedral', goles: 0 },
+    { jugador: '', equipo: 'La Catedral', goles: 0 },
+    { jugador: ' ', equipo: 'Los Guerreros', goles: 0  },
+    { jugador: '', equipo: 'El Rejunte', goles: 0  },
   ],
   'C': [
-    { jugador: 'Pedro López', equipo: 'Los Pibes', goles: 9 },
-    { jugador: 'Matías Ruiz', equipo: 'El Barrio', goles: 7 },
+    { jugador: '', equipo: 'La Catedral', goles: 0 },
+    { jugador: '', equipo: 'La Catedral', goles: 0 },
+    { jugador: '', equipo: 'La Catedral', goles: 0 },
+    { jugador: '', equipo: 'Los Pibes', goles: 0 },
+    { jugador: '', equipo: 'El Barrio', goles: 0 },
   ],
   '+38': [
-    { jugador: 'Ricardo Torres', equipo: 'Veteranos', goles: 5 },
-    { jugador: 'Mariano Rojas', equipo: 'Los Cancheros', goles: 3 },
+    { jugador: '', equipo: 'La Catedral', goles: 0 },
+    { jugador: '', equipo: 'La Catedral', goles: 0 },
+    { jugador: '', equipo: 'La Catedral', goles: 0 },
+    { jugador: '', equipo: 'Veteranos', goles: 0 },
+    { jugador: '', equipo: 'Los Cancheros', goles: 0 },
   ],
   '+45': [
-    { jugador: 'José Fernández', equipo: 'La Vieja Guardia', goles: 2 },
-    { jugador: 'Luis García', equipo: 'Veteranos FC', goles: 1 },
+    { jugador: '', equipo: 'La Catedral', goles: 0 },
+    { jugador: '', equipo: 'La Catedral', goles: 0 },
+    { jugador: '', equipo: 'La Catedral', goles: 0 },
+    { jugador: '', equipo: 'La Vieja Guardia', goles: 0 },
+    { jugador: '', equipo: 'Veteranos FC', goles: 0 },
   ]
 };
 
@@ -189,7 +256,7 @@ function mostrarGoleadores(categoria) {
         `<tr>
           <td>${g.jugador}</td>
           <td>${g.equipo}</td>
-          <td>${g.goles}</td>
+          <td>${g.goles}<0 d>
         </tr>`
       ).join('')}
     </tbody>
@@ -206,16 +273,34 @@ function volverInicio() {
 // Ejemplo de datos de partidos
 const partidosData = {
   'A': [
-    'Equipo A1 vs Equipo A2 - Fecha 10/06',
-    'Equipo A3 vs Equipo A4 - Fecha 10/06',
-    'Equipo A5 vs Equipo A6 - Fecha 11/06'
+    'La Noite vs La Nueva Guardia - Fecha 09/06',
+    'As de Basto vs Motorhead - Fecha 09/06',
+    'El JRS vs Jardin Espacial - Fecha 09/06'
   ],
   'B': [
-    'Equipo B1 vs Equipo B2 - Fecha 11/06',
-    'Equipo B3 vs Equipo B4 - Fecha 12/06',
-    'Equipo B5 vs Equipo B6 - Fecha 13/06'
+    // 'Equipo B1 vs Equipo B2 - Fecha 11/06',
+    // 'Equipo B3 vs Equipo B4 - Fecha 12/06',
+    // 'Equipo B5 vs Equipo B6 - Fecha 13/06'
+    'No hay partidos de esta categoria, volve mañana!'
   ],
-  // Agregá tus otras categorías...
+  'C': [
+    // 'Equipo A1 vs Equipo A2 - Fecha 10/06',
+    // 'Equipo A3 vs Equipo A4 - Fecha 10/06',
+    // 'Equipo A5 vs Equipo A6 - Fecha 11/06'
+    'No hay partidos de esta categoria, volve mañana!'
+  ],
+  '+38': [
+    // 'Equipo A1 vs Equipo A2 - Fecha 10/06',
+    // 'Equipo A3 vs Equipo A4 - Fecha 10/06',
+    // 'Equipo A5 vs Equipo A6 - Fecha 11/06'
+    'No hay partidos de esta categoria, volve mañana!'
+  ],
+  '+45': [
+    // 'Equipo A1 vs Equipo A2 - Fecha 10/06',
+    // 'Equipo A3 vs Equipo A4 - Fecha 10/06',
+    // 'Equipo A5 vs Equipo A6 - Fecha 11/06'
+    'No hay partidos de esta categoria, volve mañana!'
+  ],
 };
 
 function mostrarPartidos(categoria) {
